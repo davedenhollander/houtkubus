922917507aa7b80a82a65244b5376ac4c5fb6de8
Niels Steenman <ncsteenman@gmail.com>
2016-12-15 19:57:29 +0100

    Install Smarty template

---
+++ resources/views/app/templates/nav.blade.php
@@ -0,0 +1,15 @@
+<li>
+    <a href="{{ url('') }}">HOME</a>
+</li>
+<li>
+    <a href="{{ url('') }}">VLOEREN</a>
+</li>
+<li>
+    <a href="{{ url('') }}">CATALOGUS</a>
+</li>
+<li>
+    <a href="{{ url('') }}">CONTACT</a>
+</li>
+<li>
+    <a href="{{ url('') }}" class="small">WERKEN BIJ HK</a>
+</li>
